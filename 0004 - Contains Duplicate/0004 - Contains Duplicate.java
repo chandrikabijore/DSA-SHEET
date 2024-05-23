1b27a1a10812a6c678b4a8aeef4f2daa5d8c3f9d
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-05-23 17:40:25 +0530

    Create 0004 - Contains Duplicate.java

---
+++ 0004 - Contains Duplicate/0004 - Contains Duplicate.java
@@ -0,0 +1,36 @@
+package com.chandrikabijore;
+
+public class SHEET_0004_Contains_Duplicate
+{
+    public static void main(String[] args)
+    {
+        Solution sol = new Solution();
+        int[] testCase1 = {1, 2, 3, 4};
+        int[] testCase2 = {1, 2, 3, 1};
+        int[] testCase3 = {1, 2, 3, 1, 1, 3, 4, 4};
+        int[] testCase4 = {};
+
+        System.out.println(sol.ContainsDuplicate(testCase1));
+        System.out.println(sol.ContainsDuplicate(testCase2));
+        System.out.println(sol.ContainsDuplicate(testCase3));
+        System.out.println(sol.ContainsDuplicate(testCase4));
+    }
+}
+
+class Solution
+{
+    public boolean ContainsDuplicate(int[] nums)
+    {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
+
