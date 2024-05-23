2634d9fbb0bfb2054191cdc545b9c79d45a33952
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-05-23 17:44:52 +0530

    Time: 21 ms (9.36%), Space: 55.04 MB (89.37%) - LeetHub

--- 0004 - Contains Duplicate/0004 - Contains Duplicate.java
+++ 0004 - Contains Duplicate/0004 - Contains Duplicate.java
@@ -22,7 +22,7 @@ class Solution
     public boolean ContainsDuplicate(int[] nums)
     {
         int n = nums.length;
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < n - 1; i++)  
         {
             for (int j = i + 1; j < n; j++)
             {
