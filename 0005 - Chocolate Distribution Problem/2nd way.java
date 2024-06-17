6f97f080b53559babc5e986c27ea92a01f469432
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-06-17 12:16:47 +0530

    Create 2nd way.java

---
+++ 0005 - Chocolate Distribution Problem/2nd way.java
@@ -0,0 +1,29 @@
+// JAVA Code For Chocolate Distribution Problem
+package com.YouCanBro;
+
+import java.util.Arrays;
+public class SHEET_0005_ChocolateDistributionProblem
+{
+    public static void main(String[] args)
+    {
+        int[] a = {3, 4, 1, 9, 56, 7, 9, 12};
+        int n = a.length;
+        int m = 5;
+        System.out.print(Chocolate(a, n, m));
+    }
+
+    static int Chocolate(int[] a, int n, int m)
+    {
+        Arrays.sort(a);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i+m-1 < n; i++)
+        {
+            int diff = a[i+m-1] - a[i];
+            if(diff < min)
+            {
+                min = diff;
+            }
+        }
+        return min;
+    }
+}
