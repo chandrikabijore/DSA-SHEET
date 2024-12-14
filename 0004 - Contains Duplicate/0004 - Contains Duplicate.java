3ca404701d802b712bc8d2c2dcbdd191b971bb51
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-12-14 19:05:22 +0530

    LEET 14/12

--- 0004 - Contains Duplicate/0004 - Contains Duplicate.java
+++ 0004 - Contains Duplicate/0004 - Contains Duplicate.java
@@ -8,7 +8,7 @@ public class SHEET_0004_Contains_Duplicate
         int[] testCase1 = {1, 2, 3, 4};
         int[] testCase2 = {1, 2, 3, 1};
         int[] testCase3 = {1, 2, 3, 1, 1, 3, 4, 4};
-        int[] testCase4 = {};
+        int[] testCase4 = {};  
 
         System.out.println(sol.ContainsDuplicate(testCase1));
         System.out.println(sol.ContainsDuplicate(testCase2));
